796dc47ee40ec7a07596137788e42489b7bbd831
alamin0561464977 <amin23435678901315@gmail.com>
2022-04-13 05:21:52 +0300

    added SignUp

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
+import auth from '../../firebase.init';
 import './SignUp.css'
 
 const SignUp = () => {
@@ -7,7 +9,10 @@ const SignUp = () => {
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState('');
-    console.log(email, password, confirmPassword)
+    const navigate = useNavigate();
+
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth)
+    console.log(user)
 
     const handleEmailBlur = (event) => {
         setEmail(event.target.value)
@@ -18,22 +23,27 @@ const SignUp = () => {
     const handleConfirmPasswordBlur = (event) => {
         setConfirmPassword(event.target.value)
     }
-    // const handleErrorBlur = (event) => {
-    //     console.log(event)
-    // }
-    const handleCreatUsr = (event) => {
+    if (user) {
+        navigate('/')
+    }
+    const handleCreatUser = (event) => {
         event.preventDefault()
         setError('')
         if (password !== confirmPassword) {
-            setError('Your to Passwords did not match')
-            return
+            setError('Your two Passwords did not match')
+            return;
         }
+        if (password.length < 6) {
+            setError('Password must be 6 characters or longer')
+            return;
+        }
+        createUserWithEmailAndPassword(email, password)
     }
     return (
         <div className='form-container'>
             <div>
                 <h2 className='form-title'>Sign Up</h2>
-                <form onSubmit={handleCreatUsr}>
+                <form onSubmit={handleCreatUser}>
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
                         <input onBlur={handleEmailBlur} placeholder='Email' type="email" name="email" id="" required />
