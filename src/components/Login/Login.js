c2691e4c8d8b9702192588505270dcb614b91748
alamin0561464977 <amin23435678901315@gmail.com>
2022-04-13 05:58:59 +0300

    added Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Login.css'
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const navigate = useNavigate();
+
+    const handleEmailBlur = (event) => {
+        setEmail(event.target.value)
+    }
+    const handlePasswordBlur = (event) => {
+        setPassword(event.target.value)
+    }
+    if (user) {
+        navigate('/shop');
+    }
+    const handleUserSignIn = (event) => {
+        event.preventDefault();
+        signInWithEmailAndPassword(email, password)
+
+    }
+
     return (
         <div className='form-container'>
             <div>
                 <h2 className='form-title'>Login</h2>
-                <form>
+                <form onSubmit={handleUserSignIn}>
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
-                        <input placeholder='Email' type="email" name="email" id="" required />
+                        <input onBlur={handleEmailBlur} placeholder='Email' type="email" name="email" id="" required />
                     </div>
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
-                        <input placeholder='Password' type="password" name="password" id="" required />
+                        <input onBlur={handlePasswordBlur} placeholder='Password' type="password" name="password" id="" required />
                     </div>
+                    <p style={{ color: 'red' }}>{error?.message}</p>
+                    {
+                        loading && <p>Loding...</p>
+                    }
                     <input className='form-submit' type="submit" value="Login" />
                 </form>
                 <p>
@@ -23,7 +54,6 @@ const Login = () => {
                 </p>
                 <div className='div-or'>
                     <div className='div-1'>
-
                     </div>
                     <p>or</p>
                     <div className='div-1'>
